d52ddeeb40b9b8acab789d23f9d7cb95147a338a
Ryan Kelley <rskelley9@gmail.com>
2013-11-06 10:54:38 -0600

    added id counter function, objects for id, board and postit

---
+++ example.js
@@ -0,0 +1,25 @@
+var index = 2;
+
+var PostIt = function(e) {
+    var $post_it = $("#master").clone();
+    $post_it.css({'top': e.pageY + 'px', 'left': e.pageX + 'px', display: 'block', 'z-index': ++index})
+    .draggable({ handle: ".header" });
+    $post_it.on("click", function(e){
+      e.stopPropagation();
+    });
+    $post_it.on("mousedown", function(e){
+      $(this).css({'z-index': ++index});
+    });
+    $("#board").append($post_it);
+};
+
+$("#board").on("click", function(e){
+  console.log(e.target)
+  if (e.target.className != "content" &&
+      e.target.className != "header" &&
+      e.target.className != "post-it"){
+    myPostIt = new PostIt(e)
+  }
+});
+
+
--- post-it.js
+++ post-it.js
@@ -1,23 +1,54 @@
-var index = 2;
 
-var PostIt = function(e) {
-    var $post_it = $("#master").clone();
-    $post_it.css({'top': e.pageY + 'px', 'left': e.pageX + 'px', display: 'block', 'z-index': ++index})
-    .draggable({ handle: ".header" });
-    $post_it.on("click", function(e){
-      e.stopPropagation();
-    });
-    $post_it.on("mousedown", function(e){
-      $(this).css({'z-index': ++index});
-    });
-    $("#board").append($post_it);
-};
 
-$("#board").on("click", function(e){
-  console.log(e.target)
-  if (e.target.className != "content" &&
-      e.target.className != "header" &&
-      e.target.className != "post-it"){
-    myPostIt = new PostIt(e)
-  }
+function Id(){
+  this.idCounter = 0
+}
+
+Id.prototype.incrementId = function(){
+  this.idCounter++
+}
+
+function Postit(id){
+  this.content = null
+  this.id = id
+
+  // this.init = function(){
+  //   this.id = generateId();
+  // }
+
+
+}
+
+
+Postit.prototype.render = function(){
+  $("#master").clone()
+
+}
+
+function Board(){
+
+}
+
+
+$(document).ready(function(){
+     newid = new Id()
+
+  $("body#board").on("click", function(){
+
+    newid.incrementId();
+
+    postItId = newid.idCounter
+
+    postIt = new Postit(postItId)
+
+    // postIt.render
+
+    console.log(postItId)
+
+
+
+  })
+
+
 });
+
